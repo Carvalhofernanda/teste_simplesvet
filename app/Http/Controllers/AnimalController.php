a5ccb0f9ae58ff2a852c451564237a3c2138dde3
Carvalhofernanda <nandacsena16@gmail.com>
2021-08-15 21:26:49 -0300

    ajustes controlador

--- app/Http/Controllers/AnimalController.php
+++ app/Http/Controllers/AnimalController.php
@@ -7,5 +7,18 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class AnimalController extends Controller
 {
-    
+    public function import(Request $request){
+        
+        $animal=$this->obj->import([
+            'Id'=>$request->Id,
+            'IdCliente'=>$request->IdCliente,
+            'Nome'=>$request->Nome,
+            'Raca'=>$request->Raca,
+            'Especie'=>$request->Especie,
+            'HistoricoClinico'=>$request->HistoricoClinico,
+            'Nascimento'=>$request->Nascimento
+        ]);
+        return response($request);   
+
+    }
 }
--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ClienteController extends Controller
 {
-    //
+    public function import(Request $request){
+        
+        $animal=$this->obj->import([
+            'Id'=>$request->Id,
+            'Nome'=>$request->Nome,
+            'Telefone1'=>$request->Telefone1,
+            'Telefone2'=>$request->Telefone2,
+            'Email'=>$request->Email
+        ]);
+         return response($request);   
+    }
 }
